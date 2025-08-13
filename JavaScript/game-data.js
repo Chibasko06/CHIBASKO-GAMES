1318a750cd775da99f09027d2b02a55bb77f586b
mchiban <mohammed.chibanibahi@gmail.com>
2025-08-14 01:40:10 +0200

    ajt jeux d'action

--- JavaScript/game-data.js
+++ JavaScript/game-data.js
@@ -55,5 +55,93 @@ const games = {
         date_sortie: "27 juin 2025",
         technologie: "HTML5 (Unity WebGL)",
         description: "Armé d'un blaster tirant des projectiles colorés, vous devez tenir bon face à une horde implacable dans Stop Zombies. Chaque créature morte-vivante ne peut être éliminée qu'avec une balle de sa couleur ; la vivacité d'esprit et des réflexes aiguisés sont donc votre meilleure défense. Face à la horde grandissante, votre survie dépend de votre vigilance et de votre intelligence pour empêcher"
+    },
+    duel_of_builders: {
+        title: "Duel of builders",
+        url: "https://html5.gamemonetize.co/hhqdm1lz6isab49dpaaaecxxkr465hs0/",
+        developpeur: "Hexapp",
+        date_sortie: "11 décembre 2023",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Duel of Builders est un jeu de duel génial. Combattez avec un ami ou contre l'ordinateur ! Prêt pour ce duel amusant ? Joueur 1 W, A, S, D : Mouvement R : Changer d'arme Espace : Attaquer Joueur 2 Touches fléchées : Mouvement O : Changer d'arme P : Attaquer"
+    },
+    off_road_motocross: {
+        title: "Off Road Motocross",
+        url: "https://html5.gamemonetize.co/vcecgonbw7uvc92nrt8xgwx9meouuqtz/",
+        developpeur: "Chif",
+        date_sortie: "30 juin 2025",
+        technologie:"HTML5 (Unity WebGL)",
+        description: "Le motocross tout-terrain est un monde palpitant de vitesse et d'adrénaline. Pilotez des circuits dangereux, surmontez les obstacles et améliorez vos véhicules pour devenir le roi de la route. Pilotez une moto puissante, réalisez des cascades et améliorez vos compétences dans ce jeu de course captivant. Ressentez la vitesse et la liberté dans ce jeu addictif."
+    },
+    dead_seek: {
+        title: "Dead Seek",
+        url: "https://html5.gamemonetize.co/zt2tti9k25tgwbwmnacqoxz6l921m956/",
+        developpeur: "FPDA",
+        date_sortie: "01 juillet 2025",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Plongez dans un jeu d'arcade explosif et bourré d'action où chaque tir est un combat pour la survie ! Affrontez des hordes de zombies implacables grâce à un large arsenal d'armes et améliorez-les pour une puissance de feu maximale. Découvrez de nouvelles tactiques et explorez des zones dangereuses grouillant d'infectés. Avec des graphismes éclatants, un gameplay palpitant et une action ininterrompue, vous ne vous ennuierez jamais. Êtes-vous prêt à relever le défi ?"
+    },
+    stickman_street_fighter_3d: {
+        title: "Stickman Street Fighter 3D",
+        url: "https://html5.gamemonetize.co/r5tug67vf8lprllpnawokwa3p0xddr45/",
+        developpeur: "Kiz10",
+        date_sortie: "15 janvier 2020",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "C'est l'heure du combat dans KZ10 ! Choisissez votre stickman préféré et réalisez des combos incroyables pour terrasser vos adversaires ! Encore plus impressionnant : soyez celui qui, au milieu, met la pression à tout le monde. Dans Stickman Ultimate StreetFighter 3D, un jeu de combat à réaction incroyable, vous devez agir très vite pour donner coups de poing et coups de pied au timing parfait et éviter de vous faire tabasser par des centaines d'ennemis. Terminez 100 niveaux pour débloquer le mode sans fin. Différents types d'ennemis et plus de 20 armes sont disponibles."
+    },
+    stickman_slash: {
+        title: "Stickman Slash",
+        url: "https://html5.gamemonetize.co/dimgvlqk0miw677p6zv1snl4u3491l3i/",
+        developpeur: "Kiz10",
+        date_sortie: "12 août 2025",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Découvrez l'univers de l'un de vos jeux de combat préférés, avec Stickman comme héros principal ! Une physique avancée et des graphismes immersifs propulsent cette expérience vers un tout autre niveau !"
+    },
+    stickman_sniper: {
+        title: "Stickman Sniper",
+        url: "https://html5.gamemonetize.co/pnu56ck5blbnesweiw2z1ule6o530x9e/",
+        developpeur: "Kiz10",
+        date_sortie: "07 août 2025",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Hautement entraînés, ces jeux mettent à l'honneur les tireurs d'élite à la visée la plus meurtrière. Dans ces jeux de sniper, l'objectif est d'éviter d'être détecté et d'utiliser votre entraînement spécialisé pour infiltrer, observer et éliminer les troupes ennemies à longue distance avant même qu'elles ne vous repèrent."
+    },
+    cod_duty_call_fps: {
+        title: "COD Duty Call FPS",
+        url: "https://html5.gamemonetize.co/rc0aptuuleju6xnlpeds38lisqpz1hzu/",
+        developpeur: "BreymanGames",
+        date_sortie: "2 juillet 2025",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Plongez au cœur d'un champ de bataille déchiré par la guerre dans COD Duty Call FPS, un jeu de tir à la première personne effréné qui plonge les joueurs au cœur d'une guerre moderne. Dans un futur proche, vous incarnez un agent d'élite des forces spéciales déployé dans des régions instables, des zones de guerre urbaines et des installations ennemies secrètes, dans une campagne explosive et immersive. COD Duty Call FPS offre une expérience riche en adrénaline où vos réflexes, votre tactique et votre précision détermineront votre survie. Le jeu propose une campagne solo captivante qui suit une mission aux enjeux élevés pour empêcher une catastrophe mondiale."
+    },
+    forces_armees_io: {
+        title: "Forces Armées.io",
+        url: "https://html5.gamemonetize.co/31sze1xgnq00dw16hvmett3re4q2qx9w/",
+        developpeur: "Lich.Games",
+        date_sortie: "19 février 2024",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Bienvenue sur ArmedForces.io, où action et adrénaline, tirs intenses et aventures palpitantes vous attendent ! Lancez-vous dans une mission pour localiser et éliminer les ennemis disséminés sur la carte. Chaque recoin recèle des menaces potentielles. Restez vigilant et utilisez un mélange de furtivité, de prouesses de chasseur et de précision pour déjouer vos adversaires."
+    },
+    freehead_skate: {
+        title: "Freehead Skate",
+        url: "https://html5.gamemonetize.co/museimxpj23qgd30oirvj1xlmc0vouem/",
+        developpeur: "R1Games.com",
+        date_sortie: "2 août 2023",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Freehead Skate est un jeu d'arcade rapide qui met les joueurs au défi de sauter par-dessus des obstacles en utilisant uniquement la tête de leur personnage. Grâce à des commandes simples et un gameplay addictif, les joueurs doivent synchroniser leurs sauts avec la tête à la perfection pour venir à bout de niveaux de plus en plus difficiles et atteindre des scores élevés."
+    },
+    dunk_slash: {
+        title: "Dunk Slash",
+        url: "https://html5.gamemonetize.co/r5q7wmk5mdoulxc1pjzpekh8mkcyrsun/",
+        developpeur: "R1Games.com",
+        date_sortie: "19 juillet 2023",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Dunk Slash est un jeu de basket-ball captivant où les joueurs doivent couper des fils stratégiquement pour diriger le ballon vers le but. Avec des coupes limitées et des agencements de fils complexes, votre précision et votre intelligence seront mises à rude épreuve !"
+    },
+    iron_legion: {
+        title: "Iron Legion",
+        url: "https://html5.gamemonetize.co/gkeofcoqengjzxlw0b09f2qrng35egbi/",
+        developpeur: "Farkhutdinov",
+        date_sortie: "05 août 2025",
+        technologie: "HTML5 (Unity WebGL)",
+        description: "Iron Legion Plongez dans des batailles de chars épiques et modernes ! Iron Legion est un jeu d'action en ligne palpitant où vous incarnez le commandant d'un formidable véhicule de combat. Caractéristiques du jeu : * Plus de 10 modèles de chars classiques légendaires * Cartes uniques avec un terrain détaillé * Système de développement et d'amélioration des véhicules avec des mécanismes de dégâts réalistes * Batailles en équipe jusqu'à 20 joueurs en temps réel."
     }
 };
